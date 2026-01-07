c7a017bf1540f960c054ded8e5487dc2ab7926ea
thehrDev <hamzatrukayat2822@gmail.com>
2026-01-07 14:50:56 +0100

    Added the structure for customer class

---
+++ bank_action.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self,id,name,email,phone,accounts):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.accounts = accounts
+        
